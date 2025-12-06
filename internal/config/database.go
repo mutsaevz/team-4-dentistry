c65452119787c18f7ccfa1d469a721b21e16bab9
mutsaevz <mutsaev.888@gmail.com>
2025-12-06 10:56:26 +0300

    Основной скелет

---
+++ internal/config/database.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func ConnectDB() {
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	if user == "" || password == "" || host == "" || port == "" || dbname == "" {
+		log.Fatal("Одно из значений не задано")
+	}
+
+	url := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", user, password, host, port, dbname, sslmode)
+
+	conn, err := pgx.Connect(context.Background(), url)
+	if err != nil {
+		log.Fatalf("Не удалось подключиться: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	log.Println("Подключение к Supabase прошло успешно!")
+}
