a6e56cb6b9e827f9c17d8ed3779198f023b7f895
mutsaevz <mutsaev.888@gmail.com>
2025-12-07 10:14:10 +0300

    исправление некоторых ошибок и добавление репозитория для doctor

--- internal/models/doctor_models.go
+++ internal/models/doctor_models.go
@@ -2,10 +2,14 @@ package models
 
 type Doctor struct {
 	Base
-	UserID          uint   `json:"user_id" gorm:"not null;uniqueIndex"`
-	Specialization  string `json:"specialization" gorm:"not null;size:100"`
-	ExperienceYears int    `json:"experience_years" gorm:"not null;default:0"`
-	Bio             string `json:"bio,omitempty" gorm:"type:text"`
+	UserID          uint     `json:"user_id" gorm:"not null;uniqueIndex"`
+	Specializations []string `json:"specializations" gorm:"type:text[]"`
+	ExperienceYears int      `json:"experience_years" gorm:"not null;default:0"`
+	Bio             string   `json:"bio,omitempty" gorm:"type:text"`
+	AvgRating       float64  `json:"avg_rating"`
+
+	Shedules []Shedule `json:"-"`
+	Reviews  []Review  `json:"-"`
 }
 
 type DoctorCreateRequest struct {
@@ -20,4 +24,12 @@ type DoctorUpdateRequest struct {
 	Specialization  *string `json:"specialization,omitempty" validate:"omitempty,max=100"`
 	ExperienceYears *int    `json:"experience_years,omitempty" validate:"omitempty,gte=0"`
 	Bio             *string `json:"bio,omitempty" validate:"omitempty,max=2000"`
+}
+
+type DoctorQueryParams struct {
+	Specialization  string
+	ExperienceYears int
+	AvgRating       float64
+
+	FilOr bool
 }
