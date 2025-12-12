7ee06ab7dff84fcf513e5db621ad05b948d8683d
kuruevimran17 <ivahabov807@gmail.com>
2025-12-12 22:12:33 +0300

    прокинул логгеры во все слои и залогировал appointments в репозитории

--- cmd/dentistry/main.go
+++ cmd/dentistry/main.go
@@ -22,13 +22,13 @@ func main() {
 
 	db := config.SetUpDatabaseConnection(logger)
 
-	userRepo := repository.NewUserRepository(db)
-	serviceRepo := repository.NewServiceRepository(db)
-	doctorRepo := repository.NewDoctorRepository(db)
-	scheduleRepo := repository.NewScheduleRepository(db)
-	reviewRepo := repository.NewReviewRepository(db)
-	patientRecordRepo := repository.NewPatientRecordRepo(db)
-	recommendationRepo := repository.NewRecommendationRepository(db)
+	userRepo := repository.NewUserRepository(db, logger)
+	serviceRepo := repository.NewServiceRepository(db, logger)
+	doctorRepo := repository.NewDoctorRepository(db, logger)
+	scheduleRepo := repository.NewScheduleRepository(db, logger)
+	reviewRepo := repository.NewReviewRepository(db, logger)
+	patientRecordRepo := repository.NewPatientRecordRepo(db, logger)
+	recommendationRepo := repository.NewRecommendationRepository(db, logger)
 
 	if err := seed.SeedAdmin(userRepo); err != nil {
 		log.Fatalf("Не удалось заполнить административную панель: %v", err)
@@ -57,17 +57,18 @@ func main() {
 		AccessTokenTTL: time.Hour * 24,
 	}
 
-	userService := services.NewUserService(userRepo)
-	servService := services.NewServService(serviceRepo)
-	doctorService := services.NewDoctorService(doctorRepo, serviceRepo, scheduleRepo)
-	authService := services.NewAuthService(userRepo, jwtCfg)
-	scheduleService := services.NewScheduleService(scheduleRepo, doctorRepo)
-	reviewService := services.NewReviewService(reviewRepo, doctorRepo, userRepo)
-	patientRecordService := services.NewPatientRecordService(patientRecordRepo)
+	userService := services.NewUserService(userRepo, logger)
+	servService := services.NewServService(serviceRepo, logger)
+	doctorService := services.NewDoctorService(doctorRepo, serviceRepo, scheduleRepo, logger)
+	authService := services.NewAuthService(userRepo, jwtCfg, logger)
+	scheduleService := services.NewScheduleService(scheduleRepo, doctorRepo, logger)
+	reviewService := services.NewReviewService(reviewRepo, doctorRepo, userRepo, logger)
+	patientRecordService := services.NewPatientRecordService(patientRecordRepo, logger)
 	recommendationService := services.NewRecommendationService(
 		recommendationRepo,
 		userRepo,
 		serviceRepo,
+		logger,
 	)
 
 	r := gin.Default()
@@ -78,6 +79,7 @@ func main() {
 
 	transports.RegisterRoutes(
 		r,
+		logger,
 		servService,
 		userService,
 		authService,
